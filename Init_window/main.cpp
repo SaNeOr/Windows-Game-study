a17ec5916b372dbaf6dfaa4f1a73c7a216c0af1f
SaNeOr <534369503@qq.com>
2018-04-21 20:16:15 +0800

    Update main.cpp

--- Init_window/main.cpp
+++ Init_window/main.cpp
@@ -1,11 +1,65 @@
 #include<Windows.h>
+#include<time.h>
 #include<iostream>
 using namespace std;
-const string ProgramTitle = "Hello Windows";
+//const string ProgramTitle = "Hello Windows";
+
+const string APPTITLE = "Game Loop";
+HWND window;
+HDC device;
+bool gameover = false; 
+
+
+void DrawBitmap(const char *filename, int x, int y) {
+
+	HBITMAP image = (HBITMAP)LoadImage(0, filename, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+
+	BITMAP bm;
+	GetObject(image, sizeof(BITMAP), &bm);
+
+	HDC hdcImage = CreateCompatibleDC(device);
+	SelectObject(hdcImage, image);
+
+	BitBlt(
+		device,
+		x, y,
+		bm.bmWidth, bm.bmHeight,
+		hdcImage,
+		0, 0,
+		SRCCOPY);
+
+	DeleteDC(hdcImage);
+	DeleteObject((HBITMAP)image);
+
+}
+
+bool Game_Init() {
+
+	srand(time(NULL));
+
+	return 1;
+}
+
+void Game_Run() {
+	if (gameover == true) return;
+
+	RECT rect;
+	GetClientRect(window, &rect);
+
+	int x = rand() % (rect.right - rect.left);
+	int y = rand() % (rect.bottom - rect.top);
+	DrawBitmap("c.bmp", x, y);
+
+}
+
+void Game_End() {
+	ReleaseDC(window, device);
+
+}
 
 //The window event callback funtion
 LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-	RECT drawRect;
+	/*RECT drawRect;
 	PAINTSTRUCT ps;
 	HDC hdc;
 
@@ -16,13 +70,21 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 			int x = n * 20;
 			int y = n * 20;
 			drawRect = { x, y, x + 100, y + 20 };
-			DrawText(hdc, ProgramTitle.c_str(), ProgramTitle.length(), &drawRect, DT_CENTER);
+			DrawText(hdc, APPTITLE.c_str(), ProgramTitle.length(), &drawRect, DT_CENTER);
 		}
 		EndPaint(hWnd, &ps);
 	}
 	break;
 
 	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}*/
+
+	switch (message)
+	{
+	case WM_DESTROY:
+		gameover = true;
 		PostQuitMessage(0);
 		break;
 	}
@@ -41,9 +103,9 @@ ATOM MyRegisterClass(HINSTANCE hInstance) {
 	wc.hInstance = hInstance;
 	wc.hIcon = NULL;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
 	wc.lpszMenuName = NULL;
-	wc.lpszClassName = ProgramTitle.c_str();
+	wc.lpszClassName = APPTITLE.c_str();
 	wc.hIconSm = NULL;
 	return RegisterClassEx(&wc);
 }
@@ -52,9 +114,9 @@ ATOM MyRegisterClass(HINSTANCE hInstance) {
 bool InitInstance(HINSTANCE hInstance, int nCmdShow) {
 
 	//create a new windsow;
-	HWND hWnd = CreateWindow(
-		ProgramTitle.c_str(), //window class
-		ProgramTitle.c_str(), //title bar
+	window = CreateWindow(
+		APPTITLE.c_str(), //window class
+		APPTITLE.c_str(), //title bar
 		WS_OVERLAPPEDWINDOW,	//windows style
 		CW_USEDEFAULT, CW_USEDEFAULT, // postion of window
 		640,480,
@@ -64,10 +126,12 @@ bool InitInstance(HINSTANCE hInstance, int nCmdShow) {
 		NULL);
 
 	//was there an error creating the window?
-	if (hWnd == 0) return 0;
+	if (window == 0) return 0;
 
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
+	ShowWindow(window, nCmdShow);
+	UpdateWindow(window);
+
+	device = GetDC(window);
 
 	return 1;
 }
@@ -83,11 +147,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 														//检查程序是否已经在运行中， 
 														//然后创建主程序窗口。
 
+
 	//main message loop
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0)) { // 
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+	//while (GetMessage(&msg, NULL, 0, 0)) { // 
+	//	TranslateMessage(&msg);
+	//	DispatchMessage(&msg);
+	//}
+
+	if (!Game_Init())return 0;
+	//bool gameover = false;
+	while (!gameover) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		Game_Run();
+
 	}
+
+	Game_End();
 	return msg.wParam;
 }
